3d40f7302b29936c1a66758c2b1631ae4657fd0a
Vikrant Yadav <vikrant.y.22@gmail.com>
2015-01-04 23:31:20 +0530

    Initial commit of Emergency Services.apk

--- app/src/main/java/com/example/vikrant/emergencyservices/Police.java
+++ app/src/main/java/com/example/vikrant/emergencyservices/Police.java
@@ -51,6 +51,7 @@ public class Police extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,Settings.class));
             return true;
         }
 
